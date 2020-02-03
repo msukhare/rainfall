58afe7b112c334186567a64a8a8d8fad8cce5e77
Mark SUKHAREV <msukhare@e2r6p8.42.fr>
2020-02-03 09:18:26 +0100

    add right source to level0

--- level0/source.c
+++ level0/source.c
@@ -1,7 +1,26 @@
-int		main(int argc, char **argv)
-{
-	int	tmp;
+#define _GNU_SOURCE
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
-	tmp = atoi(argv[0]);
-	
+int			main(int argc, char **argv)
+{
+	char	*str;
+	gid_t	gid;
+	uid_t	uid;
+
+	if (atoi(argv[0]) == 423)
+	{
+		str = strdup(argv[0]);
+		gid = getegid();
+		uid = geteuid();
+		setresgid(gid, gid, gid);
+		setresuid(uid, uid, uid);
+		execv("/bin/sh", &str);
+	}
+	else
+		write(1, "No !\n", 5);
+	return (0);
 }
